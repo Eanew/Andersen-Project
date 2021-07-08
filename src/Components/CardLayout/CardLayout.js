0a4d6e47e29e13a8d660f72b7f2f5431fae13fbe
Alexander Petrov <H.Eanew@yandex.ru>
2021-07-08 17:32:58 +0300

    Update CardLayout.js

--- src/Components/CardLayout/CardLayout.js
+++ src/Components/CardLayout/CardLayout.js
@@ -2,19 +2,15 @@ import { Container, Row, Col } from "react-bootstrap";
 import Card from "./../../Components/Card/Card";
 import "./CardLayout.scss";
 
-function CardLayout(props) {
+function CardLayout({ cards }) {
 	return (
 		<Container>
 			<Row className="my-5">
-				{props.cards.map((item, idx) => (
-					<Col md={6} lg={4} xl={3} className="mb-4">
+				{cards && cards.map((card) => (
+					<Col key={cards.id} md={6} lg={4} xl={3} className="mb-4">
 						<Card
-							imgPath={"https://image.tmdb.org/t/p/original" + item.backdrop_path}
-							title={item.title}
-							text={item.overview}
-							buttonLink={item.buttonLink}
-							isFavorite={item.isFavorite}
-							id={item.id}
+							data={cards}
+							isFavorite={Boolean(Math.round(Math.random()))}
 						/>
 					</Col>
 				))}
